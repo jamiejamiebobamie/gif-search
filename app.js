9677eacb1b8b5a88bf32550424d44580711800d4
jamiejamiebobamie <jmccrory@vt.edu>
2018-08-31 14:10:54 -0700

    giphy search initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+var http = require('http');
+var giphy = require('giphy-api')();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    giphy.search(req.query.term, function (err, response) {
+     res.render('home', {gifs: response.data})
+   });
+ });
+
+app.listen(3000, function () {
+  console.log('Gif Search listening on port localhost:3000!');
+});
+
+//around step 3 things started to break down...
